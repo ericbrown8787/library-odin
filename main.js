b16c7b677e75ce6cc5465baee6f534b0f96c4572
Eric Brown <eric.brown8787@gmail.com>
2022-08-14 18:14:10 -0400

    Add functions to generate HTML for book info cards

--- main.js
+++ main.js
@@ -1,38 +1,62 @@
 //Constructor
-function book(title,author,pages,read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
-    this.info = function(){
-        let readStatus;
-        if (read === true){
-            readStatus = "read";
-        }else{
-            readStatus = "not read yet"
-        }
-        return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+function book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.displayTitle = function () {
+    return `<h2 class="title">${title}</h2>`;
+  };
+  this.displayAuthor = function () {
+    return `<p class="author"><span class="info-type">Author:</span><span class="info-value"> ${author}</span></p>`;
+  };
+  this.displayPages = function () {
+    return `<p class="title"><span class="info-type">Pages:</span><span class="info-value"> ${pages}</span></p>`;
+  };
+  this.displayIfRead = function () {
+    let readStatus;
+    if (read === true) {
+      readStatus = "Yes";
+    } else {
+      readStatus = "No";
     }
+    return `<p class="read"><span class="info-type">Read:</span><span class="info-value"> ${readStatus}</span></p>`;
+  };
+
+  this.info = function () {
+    let readStatus;
+    if (read === true) {
+      readStatus = "read";
+    } else {
+      readStatus = "not read yet";
+    }
+    return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+  };
 }
 
 //
 let library = [];
+const bookContainer = document.getElementById("book-display");
+let displayedBooks = "";
 
 //Creates new books using book constructor and adds them to the library array
-function addBook(title,author,pages,read) {
-    library.push(new book(title,author,pages,read))
+function addBook(title, author, pages, read) {
+  library.push(new book(title, author, pages, read));
 }
 
 //Adding some dummy books
-addBook("A Brief History of Tax Evasion","Al Capone","8",true);
-addBook("Why Is The Cat Screaming?","Some Guy","62",false);
-addBook("Extreme Knitting For Beginners","GRANDMA.","685",false);
-addBook("Spreadsheets for Fun and Profit","Dr. X.L. Sheetz, M.D.","9946",true);
+addBook("A Brief History of Tax Evasion", "Al Capone", "8", true);
+addBook("Why Is The Cat Screaming?", "Some Guy", "62", false);
+addBook("Extreme Knitting For Beginners", "GRANDMA.", "685", false);
+addBook(
+  "Spreadsheets for Fun and Profit",
+  "Dr. X.L. Sheetz, M.D.",
+  "9946",
+  true
+);
 
 //Printing the contents of the library array
-library.forEach(
-    function (element) {
-        console.log(element.info());
-    }
-);
+library.forEach(function (book) {
+  displayedBooks += `<section class="displayedBook">${book.displayTitle()} ${book.displayAuthor()} ${book.displayPages()} ${book.displayIfRead()}</section>`;
+});
+bookContainer.innerHTML = displayedBooks;
