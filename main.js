8942e119503279a704fc47f795c0932bc310d9b0
Eric Brown <eric.brown8787@gmail.com>
2022-08-18 13:56:06 -0400

    Add function to display new book card

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-//Constructor
+//Global Variables
+let library = [];
+const bookContainer = document.getElementById("book-display");
+let indexNumCount = 0
+
+//Constructors
 function book(title, author, pages, read,indexNumber) {
   this.title = title;
   this.author = author;
@@ -12,7 +17,7 @@ function book(title, author, pages, read,indexNumber) {
     return `<p class="author"><span class="info-type">Author:</span><span class="info-value"> ${author}</span></p>`;
   };
   this.displayPages = function () {
-    return `<p class="title"><span class="info-type">Pages:</span><span class="info-value"> ${pages}</span></p>`;
+    return `<p class="pages"><span class="info-type">Pages:</span><span class="info-value"> ${pages}</span></p>`;
   };
   this.displayIfRead = function () {
     let readStatus;
@@ -35,16 +40,75 @@ function book(title, author, pages, read,indexNumber) {
   };
 }
 
-//
-let library = [];
-const bookContainer = document.getElementById("book-display");
-let indexNumCount = 0
-
-//Creates new books using book constructor and adds them to the library array
+//Adding and deleting books from library/DOM
 function addBook(title, author, pages, read) {
   let indexNumber = indexNumCount;
   indexNumCount++
   library.push(new book(title, author, pages, read,indexNumber));
+}
+
+function displayBookCard(book) {
+
+
+//Title Element
+let title = document.createElement('h2');
+title.setAttribute('class','title');
+title.innerHTML = book.title;
+
+//Author Element
+let author = document.createElement('p');
+author.setAttribute('class','author');
+let authorLabel = document.createElement('span');
+authorLabel.setAttribute('class','info-type');
+authorLabel.innerHTML='Author:';
+let authorValue = document.createElement('span');
+authorValue.setAttribute('class','info-value');
+authorLabel.innerHTML=book.author;
+author.appendChild(authorLabel);
+author.appendChild(authorValue);
+
+//Pages element
+let pages = document.createElement('p');
+pages.setAttribute('class','pages');
+let pagesLabel = document.createElement('span');
+pagesLabel.setAttribute('class','info-type');
+pagesLabel.innerHTML='Pages:';
+let pagesValue = document.createElement('span');
+pagesValue.setAttribute('class','info-value');
+pagesLabel.innerHTML=book.pages;
+pages.appendChild(pagesLabel);
+pages.appendChild(pagesValue);
+
+//If read element
+let readStatus;
+if (book.read === true) {
+  readStatus = "Yes";
+} else {
+  readStatus = "No";
+}
+let ifRead = document.createElement('p');
+ifRead.setAttribute('class','pages');
+let ifReadLabel = document.createElement('span');
+ifReadLabel.setAttribute('class','info-type');
+ifReadLabel.innerHTML='Read: ';
+let ifReadValue = document.createElement('span');
+ifReadValue.setAttribute('class','info-value');
+ifReadValue.innerHTML = readStatus;
+ifRead.appendChild(ifReadLabel);
+ifRead.appendChild(ifReadValue);
+
+//Container
+const card = document.createElement('section');
+card.setAttribute('class','book-card');
+card.setAttribute('data-index-number',`${book.indexNumber}`);
+
+card.appendChild(title);
+card.appendChild(author);
+card.appendChild(pages);
+card.appendChild(ifRead);
+
+document.getElementById("book-display").appendChild(card);
+
 }
 
 function updateDisplayedBooks(){
